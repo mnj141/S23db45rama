1396c1ea49418ac636d02db79f4260d5bfcf7ea5
mnj141 <123129651+mnj141@users.noreply.github.com>
2023-04-19 23:36:01 -0500

    updated

--- controllers/electronics.js
+++ controllers/electronics.js
@@ -63,3 +63,100 @@ exports.electronics_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
+    // for a specific Costume.
+exports.electronics_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await Electronics.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+   };
+   // for a specific Costume.
+// Handle Costume update form on PUT.
+   exports.electronics_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+   ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await Electronics.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.electronicsName)
+    toUpdate.electronicsName = req.body.electronicsName;
+    if(req.body.electronicsType) toUpdate.electronicsType = req.body.electronicsType;
+    if(req.body.electronicCost) toUpdate.size = req.body.electronicCost;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+   failed`);
+    }
+   };
+   // Handle a show one view with id specified by query
+exports.electronics_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await Electronics.findById( req.query.id)
+    res.render('electronicdetail',
+   { title: 'Electronics Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+// Handle Costume delete on DELETE.
+exports.electronics_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await Electronics.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+    };
+    // Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.electronics_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('electronicscreate', { title: 'electronics Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    // Handle building the view for updating a costume.
+// query provides the id
+exports.electronics_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await Electronics.findById(req.query.id)
+    res.render('electronicsupdate', { title: 'Electronics Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    // Handle a delete one view with id from query
+exports.electronics_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await Electronics.findById(req.query.id)
+    res.render('electronicsdelete', { title: 'Electronics Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
--- routes/electronics.js
+++ routes/electronics.js
@@ -13,4 +13,12 @@ const electronics_controlers= require('../controllers/electronics');
 var router = express.Router();
 /* GET costumes */
 router.get('/', electronics_controlers.electronics_view_all_Page );
+/* GET detail costume page */
+router.get('/detail', electronics_controlers.electronics_view_one_Page);
+/* GET create costume page */
+router.get('/create', electronics_controlers.electronics_create_Page);
+/* GET create update page */
+router.get('/update', electronics_controlers.electronics_update_Page);
+/* GET delete costume page */
+router.get('/delete', electronics_controlers.electronics_delete_Page);
 module.exports = router;
