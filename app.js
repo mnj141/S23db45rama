eae1c156654d855253ab6c3cca445842350d080e
mnj141 <123129651+mnj141@users.noreply.github.com>
2023-04-21 19:13:37 -0500

    Passport added

--- app.js
+++ app.js
@@ -4,6 +4,25 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    Account.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
+
 require('dotenv').config();
 const connectionString =
   process.env.MONGO_CON
@@ -40,6 +59,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -48,6 +76,13 @@ app.use('/board', boardRouter);
 app.use('/selector', selectorRouter);
 app.use('/resource', resourceRouter);
 
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -70,19 +105,19 @@ async function recreateDB() {
   // Delete everything
   await Electronics.deleteMany();
   let instance1 = new
-  Electronics({
-    electronicsName: "samsung", electronicsType: 'Mobile', electronicCost: 50000
-  });
-  
+    Electronics({
+      electronicsName: "samsung", electronicsType: 'Mobile', electronicCost: 50000
+    });
+
   let instance2 = new
     Electronics({
       electronicsName: "lenova", electronicsType: 'laptop', electronicCost: 75000
-  }); 
+    });
 
   let instance3 = new
     Electronics({
       electronicsName: "boat", electronicsType: 'earphones', electronicCost: 3000
-  });
+    });
 
   instance1.save().then(function (err, doc) {
     if (err) return console.error(err);
